ad52f8a5c67187992839cfd617daa363126bfaa3
Juan Reyes <juan.reyes@outlook.com>
2026-05-28 10:21:59 -0500

    Initial Commit

---
+++ Interval/IntervalApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalApp.swift
+//  Interval
+//
+//  Created by Juan Reyes on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct IntervalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IntervalTests/IntervalTests.swift
@@ -0,0 +1,19 @@
+//
+//  IntervalTests.swift
+//  IntervalTests
+//
+//  Created by Juan Reyes on 28/05/26.
+//
+
+import Testing
+@testable import Interval
+
+struct IntervalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
